83575f5ef33a6cd291c75d778c06b55fbfb41755
frias <dannielfrs@gmail.com>
2023-02-12 08:57:25 -0600

    project update

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.use(passport.initialize());
 app.use(passport.session());   // Create a session for passport
 app.use(
     cors({
-        origin: "*", // Location of the react app were connecting to
+        origin: ["https://dannielfrs.github.io", "http://localhost:3000"], // Location of the react app were connecting to
         credentials: true,
     })
 );
